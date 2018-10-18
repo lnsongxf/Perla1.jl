93abe20488039fd370bcb4bb7dac6a720224efae
Chiyoung Ahn <chiyahn@hotmail.com>
2018-10-18 07:25:24 -0700

    Add time-dependency in `AwarenessModel`

--- src/transition/solve_transition_dynamics.jl
+++ src/transition/solve_transition_dynamics.jl
@@ -1,6 +1,9 @@
 function solve_transition_dynamics(Q, f_0)
     # Solve transition dynamics given AwarenessModel object Q
     # using Krylov methods
-    sol_krylov(a) = expv(a,Q,f_0)
+    function sol_krylov(t)
+        Q.t = t
+        return expv(t,Q,f_0)
+    end
     return sol_krylov;
 end
