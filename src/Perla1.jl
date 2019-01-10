a3cc0a43b8948f4c35e7d116d5e92300b3db8d7a
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2019-01-10 15:59:54 -0800

    Add profit derivative functions in code base

--- src/Perla1.jl
+++ src/Perla1.jl
@@ -1,6 +1,7 @@
 module Perla1
 
 using SparseArrays, DifferentialEquations, LinearAlgebra, ExponentialUtilities, DiffEqOperators, Parameters, SpecialFunctions, OffsetArrays
+using ForwardDiff: jacobian, derivative
 import Parameters: @with_kw, @unpack
 
 include("transition/solve_transition_dynamics.jl")
@@ -11,6 +12,6 @@ include("demand/profit.jl")
 export solve_transition_dynamics
 export params_demand_default_base, params_demand_default_symmetric, params_demand_default_asymmetric_single_cohort, params_demand_default_asymmetric
 export demand_symmetric, demand_asymmetric_single_cohort, demand_asymmetric
-export get_profit
+export get_profit, get_profit_derivative
 
 end # module
--- test/demand/profit.jl
+++ test/demand/profit.jl
@@ -21,4 +21,26 @@
             end
         end
     end 
+    
+    @testset "Profit derivatives are non-negative below mc" begin 
+        for (params, demand) in ((params_demand_default_symmetric(), demand_symmetric),
+            (params_demand_default_asymmetric_single_cohort(), demand_asymmetric_single_cohort),
+            (params_demand_default_asymmetric(), demand_asymmetric))
+            N = sum(params.N_ks)
+            k_bar = length(params.N_ks)
+            b_bar = length(params.cohorts)
+            p = fill(1.0, k_bar, b_bar)
+            f = fill(1/(N+1)^b_bar, (N+1)^b_bar)
+
+            prices_below_mc = range(params.mc/2, stop=params.mc, length=20)
+
+            for k in 1:k_bar
+                for b in 1:b_bar
+                    profit = get_profit(demand)
+                    profit_derivative = get_profit_derivative(profit)
+                    @test all((p_i -> profit_derivative(p_i, k, b, p, f, params)).(prices_below_mc) .>= 0)
+                end
+            end
+        end    
+    end 
 end
