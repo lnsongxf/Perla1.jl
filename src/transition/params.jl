c5d6d725029f1157fe9572fe38be5ad3a1a79f00
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2019-01-14 14:40:52 -0800

    Add `set_size` that returns setsize of `f` by `params`

--- src/transition/params.jl
+++ src/transition/params.jl
@@ -4,3 +4,5 @@ params_transition_default_two_cohorts = @with_kw (θ = θ_default_two_cohorts,
     cohorts = (2, 2), ts_cohort = ts_cohort_default_two_cohorts, μ = 0.1)
 
 # ts_cohort_default_three_cohorts = [5.0; 10.0]
+
+set_size(params) = prod(params.cohorts.+ 1)
