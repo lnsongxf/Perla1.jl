2c546ae839bf34df7dc5d862c5915427ecec93d1
Arnav Sood <soodarnav01@gmail.com>
2019-06-03 13:49:03 -0700

    allow errors in weaving

--- docs/notebooks/generate_notebooks.jl
+++ docs/notebooks/generate_notebooks.jl
@@ -6,7 +6,7 @@ pkg"activate .." # activate main Perla1 repository
 fileset = readdir(@__DIR__)
 for file in fileset
     if occursin(".jmd", file)
-        Weave.notebook(file) # executes also
+        Weave.notebook(file, :pwd, -1, "--allow-errors") # executes also
         println("Weaved $file")
     end
 end
