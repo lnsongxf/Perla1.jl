0682197f9aa123997e0b7142f6ff27e60d2f328a
Arnav Sood <soodarnav01@gmail.com>
2019-06-03 11:35:06 -0700

    set up repo with our travis structure

--- docs/notebooks/generate_notebooks.jl
+++ docs/notebooks/generate_notebooks.jl
@@ -1,12 +1,12 @@
 using Pkg 
 pkg"add https://github.com/arnavs/Weave.jl" # version of Weave that supports notebook
-using Weave 
+using Weave
 
 pkg"activate .." # activate main Perla1 repository
 
 fileset = readdir(@__DIR__)
 for file in fileset
-    if occursin(".jmd", file) 
+    if occursin(".jmd", file)
         Weave.notebook(file) # executes also
         println("Weaved $file")
     end
