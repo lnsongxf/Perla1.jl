3b1a5ceadcf89b4b0e69841fc4b80c260ac72438
Arnav Sood <soodarnav01@gmail.com>
2019-06-03 11:11:04 -0700

    small fixes and params improvements

--- src/transition/solve_transition_dynamics.jl
+++ src/transition/solve_transition_dynamics.jl
@@ -1,12 +1,8 @@
 # solve transition dynamics with transition operator O! (either matrix or matrix-free)
 function solve_transition_dynamics(O!, params, f_0, T; dt = 0.25)
-    # unpack params
     @unpack θ, cohorts, ts_cohort, μ = params
-    N = cohorts[1] # assume that N_t is invariant across all t
-        
-    # definte the corresponding ODE problem
+    @assert length(unique(cohorts)) == 1 # assert that cohort sizes are invariant over t.
+    N = cohorts[1]
     prob = ODEProblem(O!,f_0,(0.0,T), params)
-    
-    # solve the model
     solve(prob, LinearExponential(krylov=:simple), tstops=union(0:dt:T, ts_cohort, T))
 end
